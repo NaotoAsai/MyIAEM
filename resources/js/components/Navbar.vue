9a86c23d7ac9b677148edef56d6d31f50f735ddc
NaotoAsai <himeringo4854@gmail.com>
2020-07-09 22:37:42 +0900

    ナビバーにユーザー名ドロップダウンメニュー追加

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -6,6 +6,29 @@
     >
       <v-app-bar-nav-icon @click="toggleSideMenu"></v-app-bar-nav-icon>
       <v-toolbar-title>即add</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+      <v-menu offset-y>
+        <template v-slot:activator="{on}">
+        <v-btn v-on="on" text>
+          UserName
+          <v-icon>mdi-menu-down</v-icon>
+        </v-btn>
+        </template>
+        <v-list>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>名前変更</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>ログアウト</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      </v-toolbar-items>
     </v-app-bar>
 </template>
 
